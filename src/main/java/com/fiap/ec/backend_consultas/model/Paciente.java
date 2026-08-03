8d0f390a9867cbf48ba83ea1f3622ec60e48623e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-03 10:34:23 -0300

    Aula 11/05 - Consultas: entidade, endpoints REST, DataLoader e reconstrucao do Medico

--- src/main/java/com/fiap/ec/backend_consultas/model/Paciente.java
+++ src/main/java/com/fiap/ec/backend_consultas/model/Paciente.java
@@ -29,6 +29,7 @@ public class Paciente {
     }
     public Long getId() { return id; }
     public String getNome() { return nome; }
+    public void setId(Long id) { this.id = id; }
     public String getCpf() { return cpf; }
     public String getEmail() { return email; }
     public String getTelefone() { return telefone; }
